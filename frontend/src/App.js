86fc7d7eaf06a9591246e14da12e56442cb801bd
Theo <purplecatfishz@aol.com>
2023-09-27 17:25:42 -0400

    adding linter to the front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 // Set the base URL for Axios requests
-axios.defaults.baseURL = 'http://localhost:8000'; // Update with your FastAPI server URL
+axios.defaults.baseURL = "http://localhost:8000"; // Update with your FastAPI server URL
 
 function App() {
-  const [message, setMessage] = useState('');
+	const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    // Make an HTTP GET request to /hello
-    axios.get('/')
-      .then(response => {
-        // Set the response data (e.g., "Hello, World!") in the state
-        setMessage(response.data.message);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }, []);
+	useEffect(() => {
+		// Make an HTTP GET request to /hello
+		axios
+			.get("/")
+			.then((response) => {
+				// Set the response data (e.g., "Hello, World!") in the state
+				setMessage(response.data.message);
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
+	}, []);
 
-  return (
-    <div className="App">
-      <h1>{message}</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>{message}</h1>
+		</div>
+	);
 }
 
 export default App;
