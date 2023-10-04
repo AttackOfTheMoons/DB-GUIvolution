18e828abbac826328f743e4e90c29abdf649bc01
Theo <purplecatfishz@aol.com>
2023-10-04 16:34:44 -0400

    fixed / issue

--- backend/main.py
+++ backend/main.py
@@ -8,9 +8,7 @@ GPT_API_KEY = env.get("GPT_API_KEY")
 if GPT_API_KEY is None:
     raise EnvironmentError("Missing environment variable: GPT_API_KEY")
 
-origins = [
-    "http://localhost:3000"
-]
+origins = ["http://localhost:3000"]
 
 app = FastAPI()
 
--- backend/scripts/lint.sh
+++ backend/scripts/lint.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-mypy --config-file .\backend\pyproject.toml .\backend
+mypy --config-file ./backend/pyproject.toml ./backend
 black backend --check
 isort --check-only backend
 ruff check backend --line-length 120 --no-fix
