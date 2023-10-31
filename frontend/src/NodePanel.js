0550cd527af7d3fc67332ef83003126ab65ddcd4
Yinru Li <y_li28@uncg.edu>
2023-10-30 20:54:59 -0400

    minor fixes and beautifications

--- frontend/src/NodePanel.js
+++ frontend/src/NodePanel.js
@@ -2,31 +2,31 @@ import React from "react";
 import { Panel } from "reactflow";
 
 const NodePanel = ({ setVariant }) => {
-	return (
-		<Panel position={"top-right"}>
-			<button
-				className="panel-button"
-				type="button"
-				onClick={() => setVariant("dots")}
-			>
-				·
-			</button>
-			<button
-				className="panel-button"
-				type="button"
-				onClick={() => setVariant("lines")}
-			>
-				—
-			</button>
-			<button
-				className="panel-button"
-				type="button"
-				onClick={() => setVariant("cross")}
-			>
-				+
-			</button>
-		</Panel>
-	);
+  return (
+    <Panel position={"top-left"}>
+      <button
+        className="panel-button"
+        type="button"
+        onClick={() => setVariant("dots")}
+      >
+        ·
+      </button>
+      <button
+        className="panel-button"
+        type="button"
+        onClick={() => setVariant("lines")}
+      >
+        —
+      </button>
+      <button
+        className="panel-button"
+        type="button"
+        onClick={() => setVariant("cross")}
+      >
+        +
+      </button>
+    </Panel>
+  );
 };
 
 export default NodePanel;
--- frontend/src/appHelperFunctions.js
+++ frontend/src/appHelperFunctions.js
@@ -2,193 +2,194 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 export const createAdjacencyList = (edges) => {
-	const adjacencyList = {};
+  const adjacencyList = {};
 
-	edges.forEach(({ source, target }) => {
-		adjacencyList[source] = adjacencyList[source] || [];
-		adjacencyList[target] = adjacencyList[target] || [];
-		adjacencyList[source].push(target);
-		adjacencyList[target].push(source);
-		// }
-	});
-	return adjacencyList;
+  edges.forEach(({ source, target }) => {
+    adjacencyList[source] = adjacencyList[source] || [];
+    adjacencyList[target] = adjacencyList[target] || [];
+    adjacencyList[source].push(target);
+    adjacencyList[target].push(source);
+    // }
+  });
+  return adjacencyList;
 };
 export const checkAllNodesConnected = (adjacencyList, nodeValues) => {
-	function depthFirstSearch(node, visited) {
-		visited[node] = true;
-		adjacencyList[node]?.forEach((neighbor) => {
-			if (!visited[neighbor]) {
-				depthFirstSearch(neighbor, visited);
-			}
-		});
-	}
+  function depthFirstSearch(node, visited) {
+    visited[node] = true;
+    adjacencyList[node]?.forEach((neighbor) => {
+      if (!visited[neighbor]) {
+        depthFirstSearch(neighbor, visited);
+      }
+    });
+  }
 
-	// Start the DFS from the first node in nodeData
-	const visitedNodes = {};
-	depthFirstSearch(nodeValues[0].id, visitedNodes);
+  // Start the DFS from the first node in nodeData
+  const visitedNodes = {};
+  depthFirstSearch(nodeValues[0].id, visitedNodes);
 
-	// Check if all nodes are connected
-	return nodeValues.every((node) => visitedNodes[node.id]);
+  // Check if all nodes are connected
+  return nodeValues.every((node) => visitedNodes[node.id]);
 };
 
 export const findClosestFromNodeBFS = (
-	callingNode,
-	nodeData,
-	adjacencyList,
+  callingNode,
+  nodeData,
+  adjacencyList
 ) => {
-	const queue = [{ node: callingNode, distance: 0 }];
-	const visited = new Set();
+  const queue = [{ node: callingNode, distance: 0 }];
+  const visited = new Set();
 
-	while (queue.length > 0) {
-		const { node, distance } = queue.shift();
-		visited.add(node.id);
+  while (queue.length > 0) {
+    const { node, distance } = queue.shift();
+    visited.add(node.id);
 
-		if (node.type === "from") {
-			if (distance > 0) {
-				return node; // Found the closest "From" node
-			}
-		}
-		if (!adjacencyList[node.id]) {
-			return null;
-		}
+    if (node.type === "from") {
+      if (distance > 0) {
+        return node; // Found the closest "From" node
+      }
+    }
+    if (!adjacencyList[node.id]) {
+      return null;
+    }
 
-		// Enqueue neighboring nodes
-		for (const neighborId of adjacencyList[node.id]) {
-			if (!visited.has(neighborId)) {
-				const neighborNode = nodeData.find((n) => n.id === neighborId);
-				neighborNode &&
-					queue.push({ node: neighborNode, distance: distance + 1 });
-			}
-		}
-	}
+    // Enqueue neighboring nodes
+    for (const neighborId of adjacencyList[node.id]) {
+      if (!visited.has(neighborId)) {
+        const neighborNode = nodeData.find((n) => n.id === neighborId);
+        neighborNode &&
+          queue.push({ node: neighborNode, distance: distance + 1 });
+      }
+    }
+  }
 
-	return null; // No "From" node found within the graph
+  return null; // No "From" node found within the graph
 };
 
 export const findFroms = (
-	selectWhereNodes,
-	nodeValues,
-	adjacencyList,
-	setNodes,
+  selectWhereNodes,
+  nodeValues,
+  adjacencyList,
+  setNodes
 ) => {
-	let fromNotFound = false;
-	selectWhereNodes.forEach((nodeNeedsFromTable) => {
-		const closestFromNode = findClosestFromNodeBFS(
-			nodeNeedsFromTable,
-			nodeValues,
-			adjacencyList,
-		);
-		const selectedTable = closestFromNode ? closestFromNode.value : "";
-		setNodes((prevNodes) =>
-			prevNodes.map((node) =>
-				node.id === nodeNeedsFromTable.id
-					? { ...node, data: { ...node.data, selectedTable } }
-					: node,
-			),
-		);
-		if (!closestFromNode) {
-			fromNotFound = true;
-		}
-	});
-	return fromNotFound;
+  let fromNotFound = false;
+  selectWhereNodes.forEach((nodeNeedsFromTable) => {
+    const closestFromNode = findClosestFromNodeBFS(
+      nodeNeedsFromTable,
+      nodeValues,
+      adjacencyList
+    );
+    const selectedTable = closestFromNode ? closestFromNode.value : "";
+    setNodes((prevNodes) =>
+      prevNodes.map((node) =>
+        node.id === nodeNeedsFromTable.id
+          ? { ...node, data: { ...node.data, selectedTable } }
+          : node
+      )
+    );
+    if (!closestFromNode) {
+      fromNotFound = true;
+    }
+  });
+  return fromNotFound;
 };
 
 const showNotification = (message, theme) => {
-	toast.info(message, {
-		position: "bottom-center",
-		autoClose: 3000,
-		hideProgressBar: false,
-		closeOnClick: true,
-		pauseOnHover: true,
-		draggable: true,
-		progress: undefined,
-		theme: theme,
-	});
+  toast.info(message, {
+    position: "bottom-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: theme,
+  });
 };
 
 export const sendQueryToServer = (nodeValues, setResultKeys, setResultData) => {
-	axios
-		.post("/queries/", { nodes: nodeValues, flavor: "postgres" })
-		.then((response) => {
-			const data = response.data;
-			if (!data) {
-				console.error("Unexpected response from server?");
-			}
-			setResultKeys(data.keys);
-			setResultData(data.data);
-		})
-		.catch((error) => {
-			const errorData = error.response.data;
-			// These errors shouldn't happen tm
-			if (!errorData || !errorData.detail) {
-				console.error("Unhandled server error.");
-				return;
-			}
-			// TODO: These errors we should display to the user.
-			if (Array.isArray(errorData.detail)) {
-				errorData.detail.forEach((err) => {
-					if (err.msg.includes("FROM cannot be an empty string")) {
-						showNotification("Please select a table in 🟦FROM.", "light");
-					}
-					if (err.msg.includes("SELECT list cannot be empty")) {
-						showNotification("Please select a column in SELECT.", "light");
-					} else {
-						showNotification(err.msg, "colored");
-					}
-				});
-			} else {
-				console.log(errorData.detail);
-				if (errorData.detail.includes("No FROM node found")) {
-					showNotification("Use 🟦FROM to select a table.", "colored");
-				} else {
-					showNotification(errorData.detail, "light");
-				}
-			}
-		});
+  axios
+    .post("/queries/", { nodes: nodeValues, flavor: "postgres" })
+    .then((response) => {
+      const data = response.data;
+      if (!data) {
+        console.error("Unexpected response from server?");
+      }
+      setResultKeys(data.keys);
+      setResultData(data.data);
+    })
+    .catch((error) => {
+      const errorData = error.response.data;
+      // These errors shouldn't happen tm
+      if (!errorData || !errorData.detail) {
+        console.error("Unhandled server error.");
+        return;
+      }
+      // TODO: These errors we should display to the user.
+      if (Array.isArray(errorData.detail)) {
+        errorData.detail.forEach((err) => {
+          if (err.msg.includes("FROM cannot be an empty string")) {
+            showNotification("Please select a table in 🟦FROM.", "light");
+          }
+          if (err.msg.includes("SELECT list cannot be empty")) {
+            showNotification("Please select a column in SELECT.", "light");
+          } else {
+            showNotification(err.msg, "colored");
+          }
+        });
+      } else {
+        console.log(errorData.detail);
+        if (errorData.detail.includes("No FROM node found")) {
+          showNotification("Use 🟦FROM to select a table.", "colored");
+        } else {
+          showNotification(errorData.detail, "light");
+        }
+      }
+    });
 };
 
 export const getIntialNodeValueByType = (type) => {
-	let initialValue;
+  let initialValue;
 
-	switch (type) {
-		case "from":
-			initialValue = "";
-			break;
-		case "where":
-			initialValue = {
-				column: "",
-				column_type: "",
-				compared_value: "",
-				comparator: "",
-			};
-			break;
-		case "select":
-			initialValue = [];
-			break;
-		default:
-			return "";
-	}
+  switch (type) {
+    case "from":
+      initialValue = "";
+      break;
+    case "where":
+      initialValue = {
+        column: "",
+        column_type: "",
+        compared_value: "",
+        comparator: "",
+      };
+      break;
+    case "select":
+      initialValue = [];
+      break;
+    default:
+      return "";
+  }
+  return initialValue;
 };
 
 export const createNewNode = (
-	nodeId,
-	type,
-	position,
-	initialValue,
-	nodeChange,
+  nodeId,
+  type,
+  position,
+  initialValue,
+  nodeChange
 ) => ({
-	id: nodeId,
-	type,
-	position,
-	data: {
-		label: `${type}`,
-		nodeValue: initialValue,
-		handleNodeValueChange: nodeChange,
-	},
+  id: nodeId,
+  type,
+  position,
+  data: {
+    label: `${type}`,
+    nodeValue: initialValue,
+    handleNodeValueChange: nodeChange,
+  },
 });
 
 export const createNewDataNode = (nodeId, type, initialValue) => ({
-	id: nodeId,
-	type,
-	value: initialValue,
+  id: nodeId,
+  type,
+  value: initialValue,
 });
